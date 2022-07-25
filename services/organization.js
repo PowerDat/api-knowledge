ffef43c68888838e967860b954641736c878d709
tapptm <phuwadet00123@gmail.com>
2022-07-25 17:30:26 +0700

    update university code

--- services/organization.js
+++ services/organization.js
@@ -5,7 +5,7 @@ async function getOrganizationGroup() {
   const rows = await db.query(
     `SELECT organization_code AS value, 
             organization_name_th AS label
-    FROM us_organization WHERE organization_parent_id = 0000`
+    FROM us_organization WHERE organization_parent_id = 0000 AND organization_code != 0000`
   );
   const data = helper.emptyOrRows(rows);
   data.map((item) => {
